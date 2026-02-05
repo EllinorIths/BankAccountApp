97efb0a57a217e75d93e6bb02f128cfdc3efc140
EllinorIths <ellinor.hoijer@iths.se>
2026-02-05 14:35:06 +0100

    Initial commit

---
+++ src/test/java/se/iths/ellinor/bankaccountapp/BankAccountAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.ellinor.bankaccountapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/se/iths/ellinor/bankaccountapp/component/testAccountComponent.java
@@ -0,0 +1,4 @@
+package se.iths.ellinor.bankaccountapp.component;
+
+public class testAccountComponent {
+}
